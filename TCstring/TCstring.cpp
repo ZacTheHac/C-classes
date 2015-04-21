09c62c707ee3d6a33633570205c6fadbfdfec8f7
ZacTheHac <alexangle9999@yahoo.com>
2015-04-21 16:17:32 -0700

    Updated to TCstring to make it more usable, and remove some broken functions

--- TCstring/TCstring.cpp
+++ TCstring/TCstring.cpp
@@ -2,10 +2,10 @@
 #include "TCstring.h"
 
 
-TCstring::TCstring(){}
-TCstring::~TCstring(){
-	delete[] array;
+TCstring::TCstring(){
+	SuperString.clear();
 }
+TCstring::~TCstring(){}//array is automatically destroyed once it leaves scope, oddly. I expected to have to remove it myself, as I made it with new, but I suppose it's an actual variable.
 
 TCstring::TCstring(const CHAR input[]){
 	addChar(input);
@@ -16,9 +16,12 @@ TCstring::TCstring(const TCHAR input[]){
 TCstring::TCstring(const wstring input){
 	addWString(input);
 }
+TCstring::TCstring(const string input){
+	addString(input);
+}
 
 
-TCstring & TCstring::operator+(const CHAR input[]){
+/*TCstring & TCstring::operator+(const CHAR input[]){
 	TCstring toRet(*this);
 	toRet.addChar(input);
 	return toRet;
@@ -37,7 +40,7 @@ TCstring & TCstring::operator+(const string input){
 	TCstring toRet(*this);
 	toRet.addString(input);
 	return toRet;
-}
+}*/
 TCstring & TCstring::operator+=(const CHAR input[]){
 	addChar(input);
 	return *this;
@@ -54,6 +57,14 @@ TCstring & TCstring::operator+=(const string input){
 	addString(input);
 	return *this;
 }
+TCstring & TCstring::operator+=(const TCstring input){
+	addTChar(input.getTCHAR());
+	return *this;
+}
+TCstring & TCstring::operator=(const TCstring & input){
+	SuperString.copy(input.SuperString);
+	return *this;
+}
 
 TCHAR & TCstring::operator[](int const index)const{
 	TCHAR toRet = SuperString.getRecord(index+1);//since 0 is the start in here.
@@ -61,46 +72,62 @@ TCHAR & TCstring::operator[](int const index)const{
 }
 
 bool TCstring::operator>(const TCstring comp)const{
-	int result = compare(comp.SuperString);
+	int result = compare(comp.SuperString, false);
 	if (result == -1)
 		return true;
 	else
 		return false;
 }
 bool TCstring::operator>=(const TCstring comp)const{
-	int result = compare(comp.SuperString);
+	int result = compare(comp.SuperString, false);
 	if ((result == -1)||(result == 0))
 		return true;
 	else
 		return false;
 }
 bool TCstring::operator<(const TCstring comp)const{
-	int result = compare(comp.SuperString);
+	int result = compare(comp.SuperString, false);
 	if ((result == 1))
 		return true;
 	else
 		return false;
 }
 bool TCstring::operator<=(const TCstring comp)const{
-	int result = compare(comp.SuperString);
+	int result = compare(comp.SuperString, false);
 	if ((result == 1) || (result == 0))
 		return true;
 	else
 		return false;
 }
 bool TCstring::operator==(const TCstring comp)const{
-	int result = compare(comp.SuperString);
+	int result = compare(comp.SuperString, false);
 	if ((result == 0))
 		return true;
 	else
 		return false;
 }
 bool TCstring::operator!=(const TCstring comp)const{
-	int result = compare(comp.SuperString);
+	int result = compare(comp.SuperString, false);
 	if ((result == 0))
 		return false;//careful, this one is reverse from the others
 	else
 		return true;
+}
+
+int TCstring::compare(const TCstring input, bool caseSensitive)const{
+	return compare(input.SuperString, caseSensitive);
+}
+int TCstring::compare(const CHAR input[], bool caseSensitive)const{
+	return compare(TCstring(input), caseSensitive);//kinda cheating, but shut up?
+}
+int TCstring::compare(const TCHAR input[], bool caseSensitive)const{
+	return compare(TCstring(input), caseSensitive);//kinda cheating, but shut up?
+}
+int TCstring::compare(const wstring input, bool caseSensitive)const{
+	return compare(TCstring(input), caseSensitive);//kinda cheating, but shut up?
+}
+int TCstring::compare(const string input, bool caseSensitive)const{
+	return compare(TCstring(input), caseSensitive);//kinda cheating, but shut up?
 }
 
 LPCWSTR TCstring::getLPCWSTR() const{
@@ -109,7 +136,7 @@ LPCWSTR TCstring::getLPCWSTR() const{
 }
 TCHAR* TCstring::getTCHAR() const{
 	int length = getLength();
-	delete[] array;
+	//delete[] array;
 	TCHAR* array = new TCHAR[length+1];
 	for (int i = 0; i < length; i++){
 		array[i] = SuperString.getRecord(i + 1);
@@ -150,6 +177,7 @@ void TCstring::resize(int newSize, bool cutFrontNotEnd){
 
 void TCstring::clear(){
 	SuperString.clear();
+	//delete[] array;
 }//deletes everything
 
 bool TCstring::empty(){
@@ -201,7 +229,7 @@ void TCstring::addString(const string input){
 	//again: identical code, but just because the functions overlap.
 }
 
-int TCstring::compare(LinkList<TCHAR> input)const{
+int TCstring::compare(LinkList<TCHAR> input, bool caseSensitive)const{
 	int length;
 	bool equalLength = false;
 	if (getLength() < input.getLength()){
@@ -218,6 +246,11 @@ int TCstring::compare(LinkList<TCHAR> input)const{
 	for (int i = 1; i <= length; i++){
 		SS = SuperString.getRecord(i);
 		inp = input.getRecord(i);
+		if(!caseSensitive){
+			SS = towupper(SS);
+			inp = towupper(inp);
+		}
+		
 		if (SS == inp){
 			continue;
 		}
--- TCstring/TCstring.h
+++ TCstring/TCstring.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "LinkList.h"
+#include "tchar.h"
+#include "string.h"
+
+
 class TCstring
 {
 public:
@@ -7,22 +11,30 @@ public:
 	TCstring(const CHAR input[]);
 	TCstring(const TCHAR input[]);
 	TCstring(const wstring input);
+	TCstring(const string input);
 	~TCstring();
-	TCstring & operator+(const CHAR input[]);
+	/*TCstring & operator+(const CHAR input[]);
 	TCstring & operator+(const TCHAR input[]);
 	TCstring & operator+(const wstring input);
-	TCstring & operator+(const string input);
+	TCstring & operator+(const string input);I cant get these to work right. may revisit.*/
 	TCstring & operator+=(const CHAR input[]);
 	TCstring & operator+=(const TCHAR input[]);
 	TCstring & operator+=(const wstring input);
 	TCstring & operator+=(const string input);
+	TCstring & operator+=(const TCstring input);
+	TCstring & operator=(const TCstring & input);
 	TCHAR & operator[](const int index)const;
 	bool operator>(const TCstring compare)const;
 	bool operator>=(const TCstring compare)const;
 	bool operator<(const TCstring compare)const;
 	bool operator<=(const TCstring compare)const;
 	bool operator==(const TCstring compare)const;
-	bool operator!=(const TCstring compare)const;
+	bool operator!=(const TCstring compare)const;//warning: all comparisons do a case-insensitive search by default. use the compare functions if you need case-sensitive
+	int compare(const TCstring input, bool caseSensitive = false)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
+	int compare(const CHAR input[], bool caseSensitive = false)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
+	int compare(const TCHAR input[], bool caseSensitive = false)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
+	int compare(const wstring input, bool caseSensitive = false)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
+	int compare(const string input, bool caseSensitive = false)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
 	LPCWSTR getLPCWSTR() const;
 	TCHAR* getTCHAR() const;
 	wstring getWString() const;
@@ -34,12 +46,12 @@ public:
 private:
 	//hmmm I wonder how I'm going to store this. I could make an array that I copy to a new one once space runs out... or...
 	LinkList<TCHAR> SuperString;//I love my LL class.
-	TCHAR* array;
+	//TCHAR* array;
 	void addTChar(const TCHAR input[]);
 	void addChar(const CHAR input[]);
 	void addWString(const wstring input);
 	void addString(const string input);
-	int compare(LinkList<TCHAR> input)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
+	int compare(LinkList<TCHAR> input, bool caseSensitive)const;//0 means they're equal, -1 means this one is bigger, 1 means the input is bigger
 
 };
 
